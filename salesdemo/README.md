14b193c7debbef35256b6908df301b726b74518a
Syed <syedhussain@us.ibm.com>
2018-11-14 21:18:34 +0000

    checking in sales demo template

---
+++ salesdemo/README.md
@@ -0,0 +1,8 @@
+## salesdemo infrastructure
+
+Created 11/14/18 by admin. for salesdemo
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ salesdemo/variables.tf
@@ -0,0 +1,46 @@
+#####################################################################
+##
+##      Created 11/14/18 by admin. for salesdemo
+##
+#####################################################################
+
+variable "aws_access_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "region" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "sales_instance_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "sales_instance_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "sales_instance_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_network_vpc_id" {
+  type = "string"
+  description = "Generated"
+}
+
