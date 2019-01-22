87ceb974e2a8e3791dccc64451d93e2f03ea2454
Syed Hussain <syedhussain@us.ibm.com>
2019-01-22 18:43:35 +0000

    checking in a new template

---
+++ demointernal/README.md
@@ -0,0 +1,8 @@
+## demointernal infrastructure
+
+Created 1/22/19 by admin. for demointernal
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ demointernal/variables.tf
@@ -0,0 +1,46 @@
+#####################################################################
+##
+##      Created 1/22/19 by admin. for demointernal
+##
+#####################################################################
+
+variable "aws_access_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "region" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "network_name_prefix" {
+  type = "string"
+  description = "Generated"
+}
+
