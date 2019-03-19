66db9f5bea7510cb806877a1b16495415343545a
Syed Hussain <syedhussain@us.ibm.com>
2019-03-19 10:51:14 +0000

    Schibsted Project

---
+++ Schbsted/README.md
@@ -0,0 +1,8 @@
+## Schbsted infrastructure
+
+Created 3/19/19 by admin. for Schbsted
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ Schbsted/variables.tf
@@ -0,0 +1,51 @@
+#####################################################################
+##
+##      Created 3/19/19 by admin. for Schbsted
+##
+#####################################################################
+
+variable "aws_access_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "region" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_Schibsted_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_Schibsted_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_Schibsted_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_key_pair_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_network_Schbsted_vpc_id" {
+  type = "string"
+  description = "Generated"
+}
+
